e02e1c3134671f5e842c0aa4f613a1e4ceaa6a81
Eric Soto <ericasoto2104@gmail.com>
2024-07-04 11:24:28 -0500

    Se agrega la base del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EjemploComponente from './components/EjemploComponente.jsx'
+
+export default function App() {
+  return (
+    <div>
+        
+        <EjemploComponente/>
+
+    </div>
+  )
+}
