e96143d8475716173e09a379323b6ef0c750a4a8
Jose G. <jose.garcia26@utp.ac.pa>
2024-07-19 10:55:45 -0500

    formatear codigo

--- src/API/useFetch.js
+++ src/API/useFetch.js
@@ -1,27 +1,26 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const useFetch = (url) => {
-const [data, setData] = useState(null)
-const [isLoading, setIsLoading] = useState(true);
-const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-useEffect(() => {
-  const fetchProducts = async () => {
-    setIsLoading(true);
-    try {
-      const res = await axios.get(url);
-      setData(res.data);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  fetchProducts();
-}, [url]);
+    fetchProducts();
+  }, [url]);
 
   return { data, isLoading, error };
-} 
+};
